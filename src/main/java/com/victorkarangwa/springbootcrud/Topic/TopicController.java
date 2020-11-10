021ff57002f67233f87a6c82e9d64d0bcca7ec30
Victor Karangwa <victorkarangwa4@gmail.com>
2020-11-10 16:26:51 +0200

    add topic service & controller

---
+++ src/main/java/com/victorkarangwa/springbootcrud/Topic/TopicController.java
@@ -0,0 +1,33 @@
+package com.victorkarangwa.springbootcrud.Topic;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicController {
+
+    @Autowired
+    private TopicService topicService;
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics(){
+        return topicService.getAllTopics();
+    }
+
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id){
+        return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST,value = "/topics")
+    public void addTopic(@RequestBody Topic topic){
+
+        topicService.add
+    Topic(topic);
+
+    }
+}
---
+++ src/main/java/com/victorkarangwa/springbootcrud/Topic/TopicService.java
@@ -0,0 +1,29 @@
+package com.victorkarangwa.springbootcrud.Topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Top studentsic("12","Java", "Best programming language ever"),
+                new Topic("23","Dart", "Used programming language ever"),
+                new Topic("34","Python", "Loved programming language ever")
+        ));
+
+    public List<Topic> getAllTopics(){
+        return topics;
+    }
+
+    public Topic getTopic(String id){
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic){
+       topics.add(topic);
+    }
+
+}
