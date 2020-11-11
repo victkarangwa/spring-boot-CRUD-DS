24838a284e297bb2708b08d790b23cb71e9c0b34
Victor Karangwa <victorkarangwa4@gmail.com>
2020-11-11 14:15:37 +0200

    add POST,PUT & DELETE routes

--- src/main/java/com/victorkarangwa/springbootcrud/Topic/TopicController.java
+++ src/main/java/com/victorkarangwa/springbootcrud/Topic/TopicController.java
@@ -26,8 +26,21 @@ public class TopicController {
     @RequestMapping(method = RequestMethod.POST,value = "/topics")
     public void addTopic(@RequestBody Topic topic){
 
-        topicService.add
-    Topic(topic);
+        topicService.addTopic(topic);
+
+    }
+
+    @RequestMapping(method = RequestMethod.PUT,value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id){
+
+        topicService.updateTopic(id, topic);
+
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE,value = "/topics/{id}")
+    public void deleteTopic(@PathVariable String id){
+
+        topicService.deleteTopic(id);
 
     }
 }
--- src/main/java/com/victorkarangwa/springbootcrud/Topic/TopicService.java
+++ src/main/java/com/victorkarangwa/springbootcrud/Topic/TopicService.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Service
 public class TopicService {
     private List<Topic> topics = new ArrayList<>(Arrays.asList(
-            new Top studentsic("12","Java", "Best programming language ever"),
+            new Topic("12","Java", "Best programming language ever"),
                 new Topic("23","Dart", "Used programming language ever"),
                 new Topic("34","Python", "Loved programming language ever")
         ));
@@ -26,4 +26,16 @@ public class TopicService {
        topics.add(topic);
     }
 
+    public void updateTopic(String id, Topic topic) {
+        for(int i =0; i<topics.size(); i++){
+            Topic t = topics.get(i);
+            if(t.getId().equals((id))){
+                topics.set(i, topic);
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(t -> t.getId().equals(id));
+    }
 }
